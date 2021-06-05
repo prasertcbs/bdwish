8f64203e1a74cbf387dfa2fa741ea67f30b4a0bd
Prasert Kanawattanachai <bluecotton.k@gmail.com>
2021-06-05 21:20:54 +0700

    fix server

--- server.js
+++ server.js
@@ -32,9 +32,15 @@ app.addHook('onClose', async (instance, done) => {
 });
 
 // Start listening.
-app.listen(process.env.PORT || 5000, (err) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+// fix fastify with heroku
+// ref: https://stackoverflow.com/questions/65997092/nodejs-fastify-crash-heruku-api
+app.listen(process.env.PORT, '0.0.0.0', (err, address) => {
+  if (err) throw err;
+  app.log.info(`server listening on ${address}`);
 });
+// app.listen(process.env.PORT || 3000, (err) => {
+//   if (err) {
+//     app.log.error(err);
+//     process.exit(1);
+//   }
+// });
